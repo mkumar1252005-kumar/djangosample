f37d5e1b6999ecf67a1e33356bf5de8c573dc4b8
mkumar1252005-kumar <mkumar.mmr1252005@gmail.com>
2026-03-02 09:52:30 +0530

    Update models.py

--- foranything/use1/models.py
+++ foranything/use1/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 class InternDetails(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=20, null=True, blank=True)
